634c84fdf5ad68e2fad42f7db409205f8ed9f17b
CableBK <dev.witchapat@gmail.com>
2024-11-04 22:03:15 +0700

    ตัวแปร 20241104

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-/*พิธีไหว้ครู 20241104*/
-fun main() {
-    println("Hello kotlin")
+///*พิธีไหว้ครู 20241104*/
+//fun main() {
+//    println("Hello kotlin")
+//}
+/*ตัวแปร 20241104*/
+fun main(){
+    val name="Cable"
+    var location:String="Thailand"
+    var score:Int=10
+    val GPA:Float=3.95F
+    var gpa=3.44F
+    var gender:Char='M'
+    println("Name : ${name}")
+    println("Location : ${location}")
+    println("My GPA is ${GPA} and my gpa is ${gpa}")
+    println("Gender : ${gender}")
 }
