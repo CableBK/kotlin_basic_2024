751196d1901e1922d99de603d9cd9a1bca6334a5
เคเบิล <cbkcontactmail@gmail.com>
2024-11-07 21:04:25 +0700

    Control flow if 20241107

--- src/Main.kt
+++ src/Main.kt
@@ -15,20 +15,35 @@
 //    println("My GPA is ${GPA} and my gpa is ${gpa}")
 //    println("Gender : ${gender}")
 //}
-/*คำนวน 20241105*/
+///*คำนวน 20241105*/
+//fun main(){
+//    val name="Cable"
+//    val GPA:Float=3.95F
+//    val gpa=3.44F
+//    var height=1.9F
+//    var weight=70
+//    println("Name : $name")
+//    println("my total GPA is ${GPA+gpa}")
+//    println("height=$height m")
+//    println("weight=$weight kg")
+//    println("body mass index is ${weight/(height*height)}")
+//    println("-----------------------")
+//    println("legend question 2+2*2")
+//    println("2+2*2 = ${2+2*2} or (2+2)*2 = ${(2+2)*2}")
+//    println("just basic math for now.")
+//}
+/*Control flow if คำนวนเกรดอย่างง่ายมาก ๆ แสดงค่าเป็น println 20241107*/
 fun main(){
-    val name="Cable"
-    val GPA:Float=3.95F
-    val gpa=3.44F
-    var height=1.9F
-    var weight=70
-    println("Name : $name")
-    println("my total GPA is ${GPA+gpa}")
-    println("height=$height m")
-    println("weight=$weight kg")
-    println("body mass index is ${weight/(height*height)}")
-    println("-----------------------")
-    println("legend question 2+2*2")
-    println("2+2*2 = ${2+2*2} or (2+2)*2 = ${(2+2)*2}")
-    println("just basic math for now.")
+    var score=75
+        if (score >= 90) {
+            println("A")
+        } else if (score >= 80) {
+            println("B")
+        } else if (score >= 70) {
+            println("C")
+        } else if (score >= 60) {
+            println("d")
+        } else {
+            println("F")
+        }
 }
